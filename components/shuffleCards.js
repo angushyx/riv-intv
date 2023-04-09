f9535e61885b5ba13ed50ac07e695bc209ba939f
Angus <94235117+angushyx@users.noreply.github.com>
2023-04-09 13:54:29 +0800

    codeing style

--- components/shuffleCards.js
+++ components/shuffleCards.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React from "react"
 import styles from "../styles/Home.module.css"
-
 
 const ShuffleCards = () => {
   return <div className={styles.shuffleCards}>ShuffleCards</div>
--- components/video.js
+++ components/video.js
@@ -6,78 +6,75 @@ const Video = () => {
   const [isPlaying, setIsPlaying] = useState(false)
   const [playbackTime, setPlaybackTime] = useState(0)
   const videoRef = useRef(null)
+  console.log(playbackTime)
 
   const handleIntersection = (entries) => {
-       const entry = entries[0]
-      if (entry.isIntersecting) {
-        setIsVisible(true)
-        setIsPlaying(true)
-      } else {
-        setIsVisible(false)
-        setPlaybackTime(0)
-        setIsPlaying(false)
-        videoRef.current.pause()
-      }
+    const entry = entries[0]
+    if (entry.isIntersecting) {
+      setIsVisible(true)
+      setIsPlaying(true)
+    } else {
+      setIsVisible(false)
+      setPlaybackTime(0)
+      setIsPlaying(false)
+      videoRef.current.pause()
+    }
   }
 
   useEffect(() => {
-      const observer = new IntersectionObserver(handleIntersection, {
-        threshold: 0.3,
-      })
-      observer.observe(videoRef.current)
+    const observer = new IntersectionObserver(handleIntersection, {
+      threshold: 0.3,
+    })
+    observer.observe(videoRef.current)
 
-      return () => {
-        observer.unobserve(videoRef.current)
-      }
+    return () => {
+      observer.unobserve(videoRef.current)
+    }
   }, [videoRef])
 
   const handleVideoClick = () => {
-      if (isVisible) {
-        if (isPlaying) {
-          setIsPlaying(false)
-          videoRef.current.pause()
-        } else {
-          setIsPlaying(true)
-          videoRef.current.play()
-        }
-      }
-  }
-
-    const handleTimeUpdate = () => {
-      if (isVisible && isPlaying) {
-        setPlaybackTime(videoRef.current.currentTime)
-      }
-    }
-
-    const handleCanPlay = () => {
-      if (isVisible && !isPlaying) {
+    if (isVisible) {
+      if (isPlaying) {
+        setIsPlaying(false)
+        videoRef.current.pause()
+      } else {
         setIsPlaying(true)
         videoRef.current.play()
       }
     }
+  }
 
-      const handleVisibilityChange = () => {
-        if (document.hidden && isPlaying) {
-          setIsPlaying(false)
-          setPlaybackTime(0)
-          videoRef.current.pause()
-          videoRef.current.currentTime = 0
-        }
-      }
+  const handleTimeUpdate = () => {
+    if (isVisible && isPlaying) {
+      setPlaybackTime(videoRef.current.currentTime)
+    }
+  }
 
- useEffect(() => {
-          document.addEventListener(" ", handleVisibilityChange)
-          return () => {
-            document.removeEventListener(
-              "visibilitychange",
-              handleVisibilityChange
-            )
-          }
- }, [isPlaying])
+  const handleCanPlay = () => {
+    if (isVisible && !isPlaying) {
+      setIsPlaying(true)
+      videoRef.current.play()
+    }
+  }
 
- console.log(isVisible,"isVisible")
- console.log(isPlaying,"isPlaying")
+  const handleVisibilityChange = () => {
+    if (document.hidden && isPlaying) {
+      setIsPlaying(false)
+      setPlaybackTime(0)
+      videoRef.current.pause()
+      videoRef.current.currentTime = 0
+    }
+  }
 
+  useEffect(() => {
+    document.addEventListener(" ", handleVisibilityChange)
+    return () => {
+      document.removeEventListener("visibilitychange", handleVisibilityChange)
+    }
+  }, [isPlaying])
+
+  console.log(isVisible, "isVisible")
+  console.log(isPlaying, "isPlaying")
 
   return (
     <div className={styles.video}>
@@ -94,8 +91,6 @@ const Video = () => {
         onCanPlay={handleCanPlay}
       />
     </div>
-
-   
   )
 }
 
