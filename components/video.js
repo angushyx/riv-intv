995255522b804799e797c7f95b62c888f2defc34
Angus <94235117+angushyx@users.noreply.github.com>
2023-04-09 14:55:45 +0800

    在修改一下點擊功能，就可以下一步了

--- components/video.js
+++ components/video.js
@@ -2,54 +2,69 @@ import { useState, useEffect, useRef } from "react"
 import styles from "../styles/Home.module.css"
 
 const Video = () => {
-  const [isVisible, setIsVisible] = useState(false)
-  const [isPlaying, setIsPlaying] = useState(false)
-  const [playbackTime, setPlaybackTime] = useState(0)
-  const videoRef = useRef(null)
-  console.log(playbackTime)
+  const [isVisible, setIsVisible] = useState(false) // 記錄影片是否顯示在畫面中
+  const [isPlaying, setIsPlaying] = useState(false) // 記錄影片是否正在播放
+  const [playbackTime, setPlaybackTime] = useState(0) // 記錄影片目前的播放時間
+  const videoRef = useRef(null) // 取得 video element 的 Ref
 
+  // 處理 IntersectionObserver 的 callback
   const handleIntersection = (entries) => {
     const entry = entries[0]
-    if (entry.isIntersecting) {
+    // 如果影片進入畫面且至少有 30% 在畫面內設定為顯示
+    if (entry.isIntersecting && entry.intersectionRatio >= 0.3) {
       setIsVisible(true)
-      setIsPlaying(true)
     } else {
-      setIsVisible(false)
-      setPlaybackTime(0)
-      setIsPlaying(false)
-      videoRef.current.pause()
+      setIsVisible(false) // 設定為不顯示
+      setPlaybackTime(videoRef.current.currentTime) // 記錄目前的播放時間
+      setIsPlaying(false) // 設定為不播放
+      videoRef.current.pause() // 暫停播放
+      videoRef.current.currentTime = 0 // 回到開始的時間
     }
   }
+
+  // 建立 IntersectionObserver，設定至少有 30% 在畫面內才算是進入畫面
 
   useEffect(() => {
     const observer = new IntersectionObserver(handleIntersection, {
       threshold: 0.3,
     })
+    // 監聽 video element
     observer.observe(videoRef.current)
 
+    // 解除監聽
     return () => {
-      observer.unobserve(videoRef.current)
+      observer.disconnect()
     }
   }, [videoRef])
 
+  // TODO:點擊功能失去?
+  // 點擊 video element 時的 callback
   const handleVideoClick = () => {
     if (isVisible) {
       if (isPlaying) {
+        console.log("失去功能?")
         setIsPlaying(false)
         videoRef.current.pause()
       } else {
         setIsPlaying(true)
-        videoRef.current.play()
+        if (playbackTime === 0) {
+          videoRef.current.play()
+        } else {
+          videoRef.current.currentTime = playbackTime
+          videoRef.current.play()
+        }
       }
     }
   }
 
+  // 影片播放時間更新的 callback
   const handleTimeUpdate = () => {
     if (isVisible && isPlaying) {
-      setPlaybackTime(videoRef.current.currentTime)
+      setPlaybackTime(0)
     }
   }
 
+  // 影片可以播放的 callback
   const handleCanPlay = () => {
     if (isVisible && !isPlaying) {
       setIsPlaying(true)
@@ -57,6 +72,7 @@ const Video = () => {
     }
   }
 
+  // 當影片不在範圍內 callback
   const handleVisibilityChange = () => {
     if (document.hidden && isPlaying) {
       setIsPlaying(false)
@@ -67,14 +83,15 @@ const Video = () => {
   }
 
   useEffect(() => {
-    document.addEventListener(" ", handleVisibilityChange)
+    document.addEventListener("visibilitychange", handleVisibilityChange)
     return () => {
       document.removeEventListener("visibilitychange", handleVisibilityChange)
     }
   }, [isPlaying])
 
-  console.log(isVisible, "isVisible")
-  console.log(isPlaying, "isPlaying")
+  // console.log(isVisible, "isVisible")
+  // console.log(isPlaying, "isPlaying")
+  console.log("playbackTime", playbackTime)
 
   return (
     <div className={styles.video}>
@@ -82,13 +99,14 @@ const Video = () => {
         ref={videoRef}
         className={styles.video}
         src="/videos/sample.mp4"
-        autoPlay={true}
+        // autoPlay={true}
         loop
         muted={true}
         controls={false}
         onClick={handleVideoClick}
         onTimeUpdate={handleTimeUpdate}
-        onCanPlay={handleCanPlay}
+        onCanPlay={handleCanPlay()}
+        currentTime={playbackTime}
       />
     </div>
   )
