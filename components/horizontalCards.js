35de7255cf0fee6611416a262264f309880816f9
Angus <94235117+angushyx@users.noreply.github.com>
2023-04-16 11:53:21 +0800

    50%

--- components/horizontalCards.js
+++ components/horizontalCards.js
@@ -1,8 +1,85 @@
-import React from 'react'
+import React, { useRef, useState } from "react"
 import styles from "../styles/Home.module.css"
+import Image from "next/image"
+import cardOne from "../public/images/Horizontal/Horizontal1.png"
+import cardTwo from "../public/images/Horizontal/Horizontal2.png"
+import cardThree from "../public/images/Horizontal/Horizontal3.png"
+import cardFour from "../public/images/Horizontal/Horizontal4.png"
+import cardFive from "../public/images/Horizontal/Horizontal5.png"
 
 const HorizontalCards = () => {
-  return <div className={styles.horizontalCards}>HorizontalCards</div>
+  const cardImages = [cardOne, cardTwo, cardThree, cardFour, cardFive]
+
+  const [isDragging, setIsDragging] = useState(false)
+  const [startX, setStartX] = useState(null)
+  const [scrollLeft, setScrollLeft] = useState(0)
+  const [scrollTarget, setScrollTarget] = useState(0)
+  const cardsWrapperRef = useRef(null)
+  const multiplier = 2
+
+  const handleMouseDown = (e) => {
+    setIsDragging(true)
+    setStartX(e.pageX - cardsWrapperRef.current.offsetLeft)
+    setScrollLeft(cardsWrapperRef.current.scrollLeft)
+    setScrollTarget(null)
+  }
+
+  const handleMouseMove = (e) => {
+    if (!isDragging) return
+    e.preventDefault()
+    const x = e.pageX - cardsWrapperRef.current.offsetLeft
+    const distance = (x - startX) * multiplier
+    cardsWrapperRef.current.scrollLeft = scrollLeft - distance
+  }
+
+  const handleMouseUp = () => {
+    setIsDragging(false)
+    if (scrollTarget === null) {
+      const currentScrollLeft = cardsWrapperRef.current.scrollLeft
+      let newScrollLeft
+      if (currentScrollLeft % 300 > 150) {
+        newScrollLeft = currentScrollLeft + 300 - (currentScrollLeft % 300)
+      } else {
+        newScrollLeft = currentScrollLeft - (currentScrollLeft % 300)
+      }
+      setScrollTarget(newScrollLeft)
+      const scrollStep = () => {
+        cardsWrapperRef.current.scrollLeft +=
+          (scrollTarget - cardsWrapperRef.current.scrollLeft) / 4
+        if (Math.abs(cardsWrapperRef.current.scrollLeft - scrollTarget) < 1) {
+          cardsWrapperRef.current.scrollLeft = scrollTarget
+        } else {
+          requestAnimationFrame(scrollStep)
+        }
+      }
+      requestAnimationFrame(scrollStep)
+    }
+  }
+
+  return (
+    <ul
+      onMouseDown={handleMouseDown}
+      onMouseMove={handleMouseMove}
+      onMouseUp={handleMouseUp}
+      onMouseLeave={handleMouseUp}
+      ref={cardsWrapperRef}
+      className={styles.shuffleCardsWrapper}
+    >
+      {cardImages.map((image, index) => (
+        <li className={styles.shuffleCard} key={index}>
+          <div className={styles.imageWrapper}>
+            <Image
+              src={image}
+              alt={`card ${index + 1}`}
+              width="1000"
+              height="1000"
+              className={styles.img}
+            />
+          </div>
+        </li>
+      ))}
+    </ul>
+  )
 }
 
 export default HorizontalCards
--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
+      <main className={styles.main}>
         <Header>
           <h1>Film maker</h1>
         </Header>
@@ -28,7 +28,9 @@ export default function Home() {
           We are a creative agency, film production,
           <br /> branded & original content creators.
         </div>
-        <HorizontalCards />
+        <div className={styles.container}>
+          <HorizontalCards />
+        </div>
       </main>
     </>
   )
